279e284e1613065c144232f71104f0a026431825
Lucas Franchi <lucas.franchi12@outlook.com>
2025-03-13 10:37:47 -0300

    Backup

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 import os
 import io
+import uuid
 import win32com.client
 import pythoncom
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 
 def convertExcelToPdf(excel_bytes):
     # Initialize COM
     pythoncom.CoInitialize()
 
+    # Generate unique IDs for temporary files
+    unique_id = str(uuid.uuid4())
+
     # Create a temporary file for the Excel file
     with io.BytesIO(excel_bytes) as excel_file:
-        temp_excel_path = 'temp_excel.xlsx'
+        temp_excel_path = f'temp_excel_{unique_id}.xlsx'
         with open(temp_excel_path, 'wb') as f:
             f.write(excel_file.getbuffer())
 
@@ -26,7 +30,7 @@ def convertExcelToPdf(excel_bytes):
         wb = excel.Workbooks.Open(os.path.abspath(temp_excel_path))
 
         # Define the temporary PDF file path
-        temp_pdf_path = 'temp_pdf.pdf'
+        temp_pdf_path = f'temp_pdf_{unique_id}.pdf'
 
         # Export as PDF
         wb.ExportAsFixedFormat(0, os.path.abspath(temp_pdf_path))
@@ -65,4 +69,4 @@ def convert():
     return send_file(io.BytesIO(pdf_bytes), download_name='output.pdf', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
